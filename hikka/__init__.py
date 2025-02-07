373acfab4d978c326643ee475c42d22f2a2d90cf
I love you <91001207+kramiikk@users.noreply.github.com>
2025-02-07 11:21:57 +0500

    Update

--- hikka/__init__.py
+++ hikka/__init__.py
@@ -3,9 +3,6 @@
 # Do not delete this file
 
 
-__author__ = "Dan Gazizullin"
 __copyright__ = "Copyright 2022, Dan Gazizullin"
 __credits__ = ["LonamiWebs", "penn5"]
 __license__ = "AGPLv3"
-__maintainer__ = "developer"
-__status__ = "Production"
