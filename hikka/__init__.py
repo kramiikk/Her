898294e8d96aab766245fcd9ffb1d15732644afa
I love you <91001207+kramiikk@users.noreply.github.com>
2025-02-12 14:22:58 +0500

    Update

--- hikka/__init__.py
+++ hikka/__init__.py
@@ -1,14 +1,9 @@
 """Just a placeholder to do relative imports"""
 
-# 🌟 Hikka, Friendly Telegram
-
-# Maintainers  | Dan Gazizullin, codrago
-# Years Active | 2018 - 2024
-# Repository   | https://github.com/hikariatama/Hikka
-
+# Heroku, Hikka, Friendly Telegram
 # Do not delete this file
 
 
 __copyright__ = "Copyright 2022, Dan Gazizullin"
-__credits__ = ["LonamiWebs", "penn5"]
+__credits__ = ["LonamiWebs", "penn5", "coddrago"]
 __license__ = "AGPLv3"
